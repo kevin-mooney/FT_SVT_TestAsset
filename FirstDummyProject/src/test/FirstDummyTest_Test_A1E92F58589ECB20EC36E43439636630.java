540ca5215c72b7c048d635ffccf795084a8ed6c9
Navanit Singh <navanit.s@hcl.com>
2019-03-06 17:08:49 +0530

    AFT Asset

---
+++ FirstDummyProject/src/test/FirstDummyTest_Test_A1E92F58589ECB20EC36E43439636630.java
@@ -0,0 +1,155 @@
+/**@generated
+ * WARNING  Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+/******************************************************************
+* Licensed Materials - Property of IBM
+* IBM Rational Web UI Test
+* Copyright IBM Corporation 2013. All Rights Reserved.
+* 
+* U.S. Government Users Restricted Rights - Use, duplication or 
+* disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+******************************************************************/
+/* TestScript.template - Suitable for collaborative code generation */
+package test;
+
+import com.ibm.rational.test.lt.datacorrelation.execution.action.VariableAction;
+import com.ibm.rational.test.lt.datacorrelation.execution.harvest.DataCorrelationVar;
+import com.ibm.rational.test.lt.datacorrelation.execution.harvest.DataVar;
+import com.ibm.rational.test.lt.datacorrelation.execution.harvest.IDCCoreVar;
+import com.ibm.rational.test.lt.datacorrelation.execution.harvest.IDataCorrelationVar;
+import com.ibm.rational.test.lt.datacorrelation.execution.proto.ProtoAdapterHandler;
+import com.ibm.rational.test.lt.execution.core.impl.LTTestScript;
+import com.ibm.rational.test.lt.execution.moeb.action.EndMoebStepBatchAction;
+import com.ibm.rational.test.lt.execution.moeb.action.MoebStepBatchAction;
+import com.ibm.rational.test.lt.execution.moeb.services.*;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebActionFailedEvent;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebFatalErrorEvent;
+import com.ibm.rational.test.lt.kernel.IDataArea;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+
+@SuppressWarnings("all")
+public class FirstDummyTest_Test_A1E92F58589ECB20EC36E43439636630 extends LTTestScript  {
+
+    static ProtoAdapterHandler pa = new ProtoAdapterHandler();
+    static {           
+        pa.addPA("com.ibm.rational.test.lt.execution.moeb.dc.MoebDataAdapter", "com.ibm.rational.test.lt.execution.moeb.action.MoebStepBatchAction");
+pa.addPA("com.ibm.rational.test.lt.datacorrelation.execution.protocol.core.CoreProtoAdapter", "coreAdapter");
+    }
+	
+	private DataVar[] vars = new DataVar[1];
+	
+    
+    
+	
+	public FirstDummyTest_Test_A1E92F58589ECB20EC36E43439636630(IContainer container, String invocationId) {
+		super(container, "FirstDummyTest", invocationId, "A1E92F58589ECB20EC36E43439636630");
+		setArmEnabled(false);
+		stopAtYourConvenience=false;
+		
+	}
+
+    public void execute() {
+    	try {
+			
+	    		if (!isScheduleRun()) setThinkMax(2000);
+	
+	if (!isScheduleRun()){
+this.addEventBehavior(new RPTEventStructure(new MoebActionFailedEvent(), new RPTContinueEvent("Playback of Mobile or Web UI Action failed"), 0, "Playback of Mobile or Web UI Action failed"));
+	this.addEventBehavior(new RPTEventStructure(new MoebFatalErrorEvent(), new RPTStopUserEvent("Playback of Mobile or Web UI step has a fatal error"), 0, "Playback of Mobile or Web UI step has a fatal error"));
+	this.addEventBehavior(new RPTEventStructure(new RPTFailVPEvent(), new RPTContinueEvent("Content Verification Point failed"), 1, "Content Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), new RPTContinueEvent("Connection failed"), 1, "Connection failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), new RPTContinueEvent("Authentication failed"), 1, "Authentication failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(), new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+	this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), new RPTContinueEvent("Failed to extract reference"), 1, "Failed to extract reference"));
+	this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), new RPTContinueEvent("Substitution failed"), 1, "Substitution failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), new RPTContinueEvent("Timeout"), 1, "Timeout"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), new RPTContinueEvent("Custom Verification Point failed"), 1, "Custom Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeAlertEvent(), new RPTContinueEvent("Custom Code reported an Alert"), 0, "Custom Code reported an Alert"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(), new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1, "Custom Code reported an unhandled exception"));
+	}
+	
+	
+	        	this.add(varAction_1(this));
+	this.add(applicationContext_1(this));
+	this.add(applicationContext_2(this));
+	this.add(applicationContext_3(this));
+			this.addFinally(new EndMoebStepBatchAction(this));
+
+	    } catch (Throwable e) {
+		     log("Test Execution: FirstDummyTest_Test_A1E92F58589ECB20EC36E43439636630 ",e);
+	    } finally {
+	        super.execute();
+		}	    
+    }
+    
+	public void preFinish() {
+		
+		super.preFinish();
+	}
+	
+	public void stop() {
+		
+		super.stop();
+	}	
+	
+	private VariableAction varAction_1(final IContainer parent) {
+
+	VariableAction vc = new VariableAction(parent, "", "A1E92F5858B44F03EC36E43439636630");	
+			vars[0] = new DataVar("RTW_WebUI_Browser_Selection", "Firefox", IDataArea.VIRTUALUSER, "IGNORE", false, false);
+
+		
+	vc.add(vars);
+	return vc;
+	
+}
+	// MoebStepBatch_Decl.template
+	public MoebStepBatchAction applicationContext_1(IContainer parent) {
+		MoebStepBatchAction stepBatch = new MoebStepBatchAction(parent,
+												  "Start www.google.co.in",
+												  "A1E92F5858C27FC0EC36E43439636630",
+												  "{\"id\":\"0\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceTestSteps\",\"steps\":[{\"uid\":\"A1E92F5858C27FC0EC36E43439636630\",\"application_package\":\"com.ibm.rational.test.mobile.android.browser\",\"application_os\":\"WebUI\",\"isWeb\":false,\"description\":\"Start <b>www.google.co.in<\\/b>\",\"action\":{\"id\":\"2\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceAction\",\"type\":\"start\",\"parameters\":[{\"name\":\"activity\",\"id\":\"3\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"com.ibm.rational.test.mobile.android.browser.BrowserMainActivity\"},{\"name\":\"rmot_kind_of_launch\",\"id\":\"4\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"playback\"},{\"name\":\"starting_url\",\"id\":\"5\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"http:\\/\\/www.google.co.in\\/?gws_rd=ssl\"},{\"name\":\"web_app_address\",\"id\":\"6\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"www.google.co.in\"},{\"name\":\"web_app_protocol\",\"id\":\"7\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"http:\\/\\/\"},{\"name\":\"web_app_context\",\"id\":\"8\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TString\",\"value\":\"\\/?gws_rd=ssl\"}]},\"sync_policy\":\"DEFAULT\",\"id\":\"1\",\"to_overriden\":false,\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceHWActionStep\",\"application_uid\":\"a5fc1322-46ad-4c76-bb3a-463f358a81de\",\"timeout\":0}],\"timeout\":10}",
+												  "/FirstDummyProject/Tests/FirstDummyTest.testsuite",
+												  null,
+												  "<default>");
+
+
+
+		return stepBatch;
+	}
+
+	// MoebStepBatch_Decl.template
+	public MoebStepBatchAction applicationContext_2(IContainer parent) {
+		MoebStepBatchAction stepBatch = new MoebStepBatchAction(parent,
+												  "Click on Edit text - www.google.co.in",
+												  "A1E92F5858D0D7A4EC36E43439636630",
+												  "{\"id\":\"0\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceTestSteps\",\"steps\":[{\"revealPref\":\"true\",\"description\":\"Click on <b>Edit text<\\/b>\",\"to_overriden\":false,\"skipScrollingIniOSDF\":false,\"application_uid\":\"a5fc1322-46ad-4c76-bb3a-463f358a81de\",\"timeout\":0,\"screenshotPref\":\"ALL\",\"uid\":\"A1E92F5858D0D7A4EC36E43439636630\",\"isWeb\":true,\"action\":{\"id\":\"2\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceAction\",\"type\":\"onclick\",\"parameters\":[{\"name\":\"enableasyncaction\",\"id\":\"3\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceParameter\",\"type\":\"TBoolean\",\"value\":\"false\"}]},\"sync_policy\":\"DEFAULT\",\"id\":\"1\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceUIActionStep\",\"object\":{\"propList\":\"{ \\\"Coordinates$array$\\\": \\\"top:321;left:301;bottom:355;right:721\\\", \\\"Geometry\\\": { \\\"height\\\": 34, \\\"width\\\": 420, \\\"x\\\": 301, \\\"y\\\": 321 }, \\\"Style$array$\\\": \\\"background-color:rgba(0, 0, 0, 0);background-repeat:repeat;color:rgba(0, 0, 0, 0.87);font-family:arial, sans-serif;font-size:16px;font-style:normal;font-weight:400;line-height:34px;margin-bottom:0px;margin-left:0px;margin-right:0px;margin-top:-37px;opacity:1;padding-bottom:0px;padding-left:0px;padding-right:0px;padding-top:0px;text-align:start;text-decoration:none solid rgba(0, 0, 0, 0.87);visibility:visible;z-index:auto;\\\", \\\"aria-autocomplete\\\": \\\"both\\\", \\\"aria-haspopup\\\": \\\"false\\\", \\\"aria-label\\\": \\\"Search\\\", \\\"autocapitalize\\\": \\\"off\\\", \\\"autocomplete\\\": \\\"off\\\", \\\"autocorrect\\\": \\\"off\\\", \\\"background-color\\\": \\\"00000000\\\", \\\"background-repeat\\\": \\\"repeat\\\", \\\"bottom\\\": 355, \\\"class\\\": \\\"gLFyf gsfi\\\", \\\"color\\\": \\\"000000DE\\\", \\\"content\\\": \\\"\\\", \\\"domainName\\\": \\\"html\\\", \\\"enabled\\\": true, \\\"font-family\\\": \\\"arial, sans-serif\\\", \\\"font-size\\\": 16, \\\"font-style\\\": \\\"normal\\\", \\\"font-weight\\\": \\\"400\\\", \\\"jsaction\\\": \\\"paste:puy29d\\\", \\\"left\\\": 301, \\\"line-height\\\": \\\"34\\\", \\\"margin-bottom\\\": 0, \\\"margin-left\\\": 0, \\\"margin-right\\\": 0, \\\"margin-top\\\": -37, \\\"maxlength\\\": \\\"2048\\\", \\\"name\\\": \\\"q\\\", \\\"opacity\\\": \\\"1\\\", \\\"padding-bottom\\\": 0, \\\"padding-left\\\": 0, \\\"padding-right\\\": 0, \\\"padding-top\\\": 0, \\\"proxyClass\\\": \\\"HtmlTextInputProxy\\\", \\\"proxyName\\\": \\\"inputtext\\\", \\\"right\\\": 721, \\\"role\\\": \\\"combobox\\\", \\\"spellcheck\\\": \\\"false\\\", \\\"tagname\\\": \\\"input\\\", \\\"text-align\\\": \\\"start\\\", \\\"text-decoration\\\": \\\"none solid rgba(0, 0, 0, 0.87)\\\", \\\"title\\\": \\\"Search\\\", \\\"top\\\": 321, \\\"type\\\": \\\"text\\\", \\\"visibility\\\": \\\"visible\\\", \\\"visible\\\": true, \\\"xpath\\\": \\\"135\\\", \\\"xpathProp\\\": \\\"\\\\\\/\\\\\\/body\\\\\\/div\\\\\\/div[3]\\\\\\/form\\\\\\/div[2]\\\\\\/div\\\\\\/div\\\\\\/div\\\\\\/div\\\\\\/input\\\", \\\"z-index\\\": \\\"auto\\\" }\",\"id\":\"4\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceUIObject\",\"type\":\"html.inputtext\"}}],\"timeout\":10}",
+												  null,
+												  null,
+												  "<default>");
+
+
+
+		return stepBatch;
+	}
+
+	// MoebStepBatch_Decl.template
+	public MoebStepBatchAction applicationContext_3(IContainer parent) {
+		MoebStepBatchAction stepBatch = new MoebStepBatchAction(parent,
+												  "Close browser window - www.google.co.in",
+												  "A1E92F5859300EA7EC36E43439636630",
+												  "{\"id\":\"0\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceTestSteps\",\"steps\":[{\"uid\":\"A1E92F5859300EA7EC36E43439636630\",\"revealPref\":\"true\",\"isWeb\":true,\"description\":\"<b>Close<\\/b> browser window\",\"action\":{\"id\":\"2\",\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceAction\",\"type\":\"close\",\"parameters\":[]},\"sync_policy\":\"DEFAULT\",\"id\":\"1\",\"to_overriden\":false,\"declaredClass\":\"com.ibm.rational.test.lt.core.moeb.model.transfer.testscript.DeviceHWActionStep\",\"application_uid\":\"a5fc1322-46ad-4c76-bb3a-463f358a81de\",\"timeout\":0,\"screenshotPref\":\"ALL\"}],\"timeout\":10}",
+												  null,
+												  null,
+												  "<default>");
+
+
+
+		return stepBatch;
+	}
+
+}
---
+++ PerfPrj/src/test/CompTest_Nav2_CompoundTest_A1E93F6914BF2D20EB42E53034336137.java
@@ -0,0 +1,53 @@
+/**@generated
+ * WARNING  Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+package test;
+
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.DataSub;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.IDataSub;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.ISubRule;
+import com.ibm.rational.test.lt.datacorrelation.execution.sub.SubRule;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.impl.For;
+import com.ibm.rational.test.lt.kernel.data.FeatureOptionsDataAreaCreation;
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class CompTest_Nav2_CompoundTest_A1E93F6914BF2D20EB42E53034336137
+		extends com.ibm.rational.test.lt.kernel.action.impl.CompoundTest {
+
+	public CompTest_Nav2_CompoundTest_A1E93F6914BF2D20EB42E53034336137(IContainer parent, String name) {
+		super(parent, "CompTest_Nav2", "A1E93F6914BF2D20EB42E53034336137");
+	}
+
+	public void execute() {
+
+		For loopNm_1 = Loop_1(this);
+		this.add(loopNm_1);
+		loopNm_1.addVarsToInit(null);
+
+		super.execute();
+	}
+
+	private For Loop_1(IContainer parent) {
+		For forLoop = new For(parent, "Loop1", "A1E93F691DA30650EB42E53034336137", 0, 0, 0, 200, 1, true, false, null) {
+
+			public void executeLoop() {
+
+				this.add(new test.Test_A1E58C87AC5C458DEF85E23366663430(this, "A1E93F69187EA620EB42E53034336137") {
+					public void execute() {
+						this.setRtbEnabled(false);
+						super.execute();
+					}
+				});
+
+				super.executeLoop();
+			}
+
+		};
+		return forLoop;
+	}
+
+}
