428b27cf8448a1ac39ce599bc266ad2b225377a8
jhansi.m <jhansi.m@AP-RCC--WIN75.NONPROD.HCLPNP.COM>
2019-05-06 15:32:21 +0530

    Pushed new AFT test

--- AFT_Project/src/test/Test2_Test_A1E940D898503E20EF4AB13531633735.java
+++ AFT_Project/src/test/Test2_Test_A1E940D898503E20EF4AB13531633735.java
@@ -24,8 +24,13 @@ import com.ibm.rational.test.lt.execution.core.impl.LTTestScript;
 import com.ibm.rational.test.lt.execution.moeb.action.EndMoebStepBatchAction;
 import com.ibm.rational.test.lt.execution.moeb.action.MoebStepBatchAction;
 import com.ibm.rational.test.lt.execution.moeb.services.*;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebActionFailedEvent;
+import com.ibm.rational.test.lt.execution.moeb.services.MoebFatalErrorEvent;
 import com.ibm.rational.test.lt.kernel.IDataArea;
 import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
 
 @SuppressWarnings("all")
 public class Test2_Test_A1E940D898503E20EF4AB13531633735 extends LTTestScript  {
@@ -54,7 +59,19 @@ pa.addPA("com.ibm.rational.test.lt.execution.moeb.dc.MoebDataAdapter", "com.ibm.
 	    		if (!isScheduleRun()) setThinkMax(2000);
 	
 	if (!isScheduleRun()){
-}
+this.addEventBehavior(new RPTEventStructure(new RPTFailVPEvent(), new RPTContinueEvent("Content Verification Point failed"), 1, "Content Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), new RPTContinueEvent("Connection failed"), 1, "Connection failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), new RPTContinueEvent("Authentication failed"), 1, "Authentication failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(), new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+	this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), new RPTContinueEvent("Failed to extract reference"), 1, "Failed to extract reference"));
+	this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), new RPTContinueEvent("Substitution failed"), 1, "Substitution failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), new RPTContinueEvent("Timeout"), 1, "Timeout"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), new RPTContinueEvent("Custom Verification Point failed"), 1, "Custom Verification Point failed"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeAlertEvent(), new RPTContinueEvent("Custom Code reported an Alert"), 0, "Custom Code reported an Alert"));
+	this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(), new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1, "Custom Code reported an unhandled exception"));
+	this.addEventBehavior(new RPTEventStructure(new MoebActionFailedEvent(), new RPTContinueEvent("Playback of Mobile or Web UI Action failed"), 0, "Playback of Mobile or Web UI Action failed"));
+	this.addEventBehavior(new RPTEventStructure(new MoebFatalErrorEvent(), new RPTStopUserEvent("Playback of Mobile or Web UI step has a fatal error"), 0, "Playback of Mobile or Web UI step has a fatal error"));
+	}
 	
 	
 	        	this.add(varAction_1(this));
